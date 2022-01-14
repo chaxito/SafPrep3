bf70c458ef69f0030ca3c7f6a1d061283e0a1eef
chaxito <cmmuli@safaricom.co.ke>
2022-01-14 18:04:37 +0300

    edits on form

--- js/jquery.js
+++ js/jquery.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-    $("img#design").mouseover(function(){
+    $("img#design").mouseover(function(event){
         $("body").removeClass();
-        alert("Design")      
+        alert("Design")   
+        event.preventDefault();
     });
+
+    // input form values
+    $("#form").submit(function(){
+        var inputName = $("input#name").val();
+        var inputEmail = $("input#email").val();
+        var inputMessage = $("textarea#message").val();
+
+        // $(".name").text(inputName);
+        $(".email").text(inputEmail);
+        $(".message").text(inputMessage);
+
+        $("input#submit").click(function(event){
+            alert('${inputName} we have received your message. Thank you for reaching out to us.');
+            event.preventDefault();
+        });
+
+        
+    })
+    
 });
