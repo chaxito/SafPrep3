1f1d6f3a4c1b04b63e9a90620c4e246aec73ef44
chaxito <cmmuli@safaricom.co.ke>
2022-01-14 23:56:11 +0300

    jquery form output

--- js/jquery.js
+++ js/jquery.js
@@ -1,24 +1,24 @@
-$(document).ready(function(){
-    $("img#design").click(function(event){
+$(document).ready(function () {
+    $("img#design").click(function (event) {
         $("body").removeClass();
-        $("img#design").addClass("text");   
+        $("img#design").addClass("text");
         event.preventDefault();
     });
 
     // input form values
-    $("form").submit(function(event){
+    $("form").submit(function (event) {
         var inputName = $("input#name").val();
         var inputEmail = $("input#email").val();
         var inputMessage = $("input#message").val();
         $(".name").text(inputName);
         $(".email").text(inputEmail);
         $(".message").text(inputMessage);
-
-        alert("success");
+        // Output form
+        alert("We have received your message. Thank you for reaching out to us.");
         event.preventDefault();
-      });
+    });
 
-        
-    })
+
+})
     
 });
