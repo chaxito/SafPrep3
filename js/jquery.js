bc6d21e89cef1191554252622d28a874005a0524
chaxito <cmmuli@safaricom.co.ke>
2022-01-14 23:58:38 +0300

    final form jquery edits

--- js/jquery.js
+++ js/jquery.js
@@ -10,10 +10,11 @@ $(document).ready(function () {
         var inputName = $("input#name").val();
         var inputEmail = $("input#email").val();
         var inputMessage = $("input#message").val();
+        // Output form values
         $(".name").text(inputName);
         $(".email").text(inputEmail);
         $(".message").text(inputMessage);
-        // Output form
+        
         alert("We have received your message. Thank you for reaching out to us.");
         event.preventDefault();
     });
